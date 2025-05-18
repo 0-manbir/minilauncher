28983bfba9254c8ae06df95048656d06f98f0221
0-manbir <0.manbirsingh@gmail.com>
2025-05-18 13:40:57 +0530

    add pages

---
+++ app/src/main/java/com/example/minilauncher/AppDrawerFragment.kt
@@ -0,0 +1,4 @@
+package com.example.minilauncher
+
+class AppDrawerFragment {
+}
--- app/src/main/java/com/example/minilauncher/MainActivity.kt
+++ app/src/main/java/com/example/minilauncher/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.minilauncher
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewPager : ViewPager2;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        viewPager = findViewById(R.id.viewPager)
+        val fragments = listOf(LeftFragment(), HomeFragment(), RightFragment())
+        viewPager.adapter = object : FragmentStateAdapter(this) {
+            override fun getItemCount() = fragments.size
+            override fun createFragment(position: Int) = fragments[position]
         }
+
+        viewPager.setCurrentItem(1, false)
     }
 }
